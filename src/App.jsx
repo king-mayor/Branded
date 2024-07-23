805832dc08734de87b59ae4182f35bb9bb8acdb8
king-mayor <99553727+king-mayor@users.noreply.github.com>
2024-07-23 09:03:52 -0700

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ import ScrollToTop from "./ScrollToTop";
 import Team from "./Team";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Teams from "./Teams";
 function App() {
   useEffect(() => {
     AOS.init();
